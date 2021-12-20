1b77c1f6e2a156c318f780a5eb88cd0b433d88ff
Arnab Sen <arnabsen1729@gmail.com>
2021-12-20 18:13:10 +0530

    adds utility functions

---
+++ utils/__init__.py
@@ -0,0 +1,666 @@
+RAW_DATA = [
+    {
+        "name": "Iman Kalyan Majumder",
+        "year": 2024,
+        "handle": "IKMajumder",
+    },
+    {
+        "name": "Manish Kundu",
+        "year": 2023,
+        "handle": "manish.17"
+    },
+    {
+        "name": "Sounak Majumder",
+        "year": 2023,
+        "handle": "coder_sounak"
+    },
+    {
+        "name": "Sushobhan Mahato",
+        "year": 2024,
+        "handle": "Sushobhan_4",
+    },
+    {
+        "name": "Tanmay Mahato",
+        "year": 2024,
+        "handle": "tanmay121",
+    },
+    {
+        "name": "Prince Kumar",
+        "year": 2024,
+        "handle": "Unstoppable_1",
+    },
+    {
+        "name": "Anoushka Patra",
+        "year": 2024,
+        "handle": "purpleberry3.14",
+    },
+    {
+        "name": "Vijay Vaswani",
+        "year": 2024,
+        "handle": "Vaswani786",
+    },
+    {
+        "name": "Soham Das",
+        "year": 2023,
+        "handle": "the_distortion",
+    },
+    {
+        "name": "Devanshu Chatterjee",
+        "year": 2024,
+        "handle": "1_DC",
+    },
+    {
+        "name": "Priyanshu",
+        "year": 2023,
+        "handle": "Priyanshu_0514",
+    },
+    {
+        "name": "Himanshu Pandey",
+        "year": 2022,
+        "handle": "Harry_3",
+    },
+    {
+        "name": "Priyanshi Agarwal",
+        "year": 2023,
+        "handle": "priyanshi3008",
+    },
+    {
+        "name": "Voleti Dharma Nandana Rajkumar",
+        "year": 2023,
+        "handle": "rajkumarvoleti",
+    },
+    {
+        "name": "Md Fahad",
+        "year": 2021,
+        "handle": "fahadmd16",
+    },
+    {
+        "name": "Swarnali Mondal",
+        "year": 2023,
+        "handle": "swar_am",
+    },
+    {
+        "name": "Soham Pal",
+        "year": 2023,
+        "handle": "soham0_0",
+    },
+    {
+        "name": "Adarsh Jaiswal",
+        "year": 2024,
+        "handle": "crackarsh",
+    },
+    {
+        "name": "Iman Kalyan Majumder",
+        "year": 2024,
+        "handle": "IKMajumder",
+    },
+    {
+        "name": "Avantika Singh",
+        "year": 2023,
+        "handle": "Avan1608",
+    },
+    {
+        "name": "Tanmay Mahato",
+        "year": 2024,
+        "handle": "tanmay121",
+    },
+    {
+        "name": "Vijay Vaswani",
+        "year": 2024,
+        "handle": "Vaswani786",
+    },
+    {
+        "name": "Karmanya Gupta",
+        "year": 2023,
+        "handle": "Aestroix",
+    },
+    {
+        "name": "Aishwarya Deb",
+        "year": 2023,
+        "handle": "aishwarya2109",
+    },
+    {
+        "name": "Anoushka Patra",
+        "year": 2024,
+        "handle": "purpleberry3.14",
+    },
+    {
+        "name": "SUVAM DEY",
+        "year": 2024,
+        "handle": "YedMavus",
+    },
+    {
+        "name": "Sushobhan Mahato",
+        "year": 2024,
+        "handle": "Sushobhan_4",
+    },
+    {
+        "name": "Subhadip Maji",
+        "year": 2023,
+        "handle": "atheistsoldier",
+    },
+    {
+        "name": "SIMEET NAYAN",
+        "year": 2024,
+        "handle": "kora_kagaz02",
+    },
+    {
+        "name": "Sainath Tallam",
+        "year": 2023,
+        "handle": "SainathTallam",
+    },
+    {
+        "name": "Preyas Patel",
+        "year": 2023,
+        "handle": "bbu_0078",
+    },
+    {
+        "name": "Arghyadeep Saha",
+        "year": 2024,
+        "handle": "fleabag",
+    },
+    {
+        "name": "Neeraj Pratap Hazarika",
+        "year": 2024,
+        "handle": "NeerajHazarika",
+    },
+    {
+        "name": "Subhadeep Das",
+        "year": 2024,
+        "handle": "subhadeepdas20011029",
+    },
+    {
+        "name": "Arindam Majumder",
+        "year": 2023,
+        "handle": "i_m_arin",
+    },
+    {
+        "name": "Raunak Gayen",
+        "year": 2024,
+        "handle": "RaunakGN",
+    },
+    {
+        "name": "Raunak Gayen",
+        "year": 2024,
+        "handle": "RaunakGN",
+    },
+    {
+        "name": "Anirban Sikder",
+        "year": 2023,
+        "handle": "BReaking_COde",
+    },
+    {
+        "name": "Hrittik Bania",
+        "year": 2024,
+        "handle": "Diamond_21Aug",
+    },
+    {
+        "name": "Ariv Debnath",
+        "year": 2024,
+        "handle": "ariv_10",
+    },
+    {
+        "name": "Jyoti Anjaly",
+        "year": 2023,
+        "handle": "Sirfiri",
+    },
+    {
+        "name": "Niket bahety",
+        "year": 2024,
+        "handle": "Niket_Bahety",
+    },
+    {
+        "name": "Vishwateja Rapolu",
+        "year": 2023,
+        "handle": "vishwa_teja",
+    },
+    {
+        "name": "Preyas Patel",
+        "year": 2023,
+        "handle": "bbu_0078",
+    },
+    {
+        "name": "Kulin Kumar",
+        "year": 2024,
+        "handle": "menace_54",
+    },
+    {
+        "name": "Komal Shaw",
+        "year": 2023,
+        "handle": "komal55",
+    },
+    {
+        "name": "Astha Kumari",
+        "year": 2023,
+        "handle": "asthakri50",
+    },
+    {
+        "name": "Rajdeep Ghosh",
+        "year": 2023,
+        "handle": "Rajdeep2k",
+    },
+    {
+        "name": "Debopriya Roy",
+        "year": 2024,
+        "handle": "dpriya.jsr",
+    },
+    {
+        "name": "Priyanshi Agarwal",
+        "year": 2023,
+        "handle": "priyanshi3008",
+    },
+    {
+        "name": "Prince Kumar",
+        "year": 2024,
+        "handle": "Unstoppable_1",
+    },
+    {
+        "name": "Dipanwita Rano",
+        "year": 2024,
+        "handle": "dipanwitarano",
+    },
+    {
+        "name": "Proggya Chakraborty",
+        "year": 2024,
+        "handle": "taitay",
+    },
+    {
+        "name": "Vinita Yadav",
+        "year": 2024,
+        "handle": "vinita_1701",
+    },
+    {
+        "name": "raunak kumar",
+        "year": 2024,
+        "handle": "risky_1930",
+    },
+    {
+        "name": "Monalisa Das",
+        "year": 2024,
+        "handle": "monali",
+    },
+    {
+        "name": "cheruku shailendra",
+        "year": 2024,
+        "handle": "Shailendra_cheruku",
+    },
+    {
+        "name": "Karmanya Gupta",
+        "year": 2023,
+        "handle": "Aestroix",
+    },
+    {
+        "name": "Prerna Gupta",
+        "year": 2023,
+        "handle": "ron_weasley27",
+    },
+    {
+        "name": "Pankhuri Goel",
+        "year": 2023,
+        "handle": "pankhuri_goel",
+    },
+    {
+        "name": "Kiranmoy Saha",
+        "year": 2024,
+        "handle": "saha_kiranmoy",
+    },
+    {
+        "name": "Anish De",
+        "year": 2023,
+        "handle": "anishde85",
+    },
+    {
+        "name": "Subhrajit Dey",
+        "year": 2024,
+        "handle": "Subhrajit123",
+    },
+    {
+        "name": "Sathi Naga Sai Maheswara Reddy",
+        "year": 2024,
+        "handle": "Maheswar003",
+    },
+    {
+        "name": "Reeju Bhattacharya",
+        "year": 2024,
+        "handle": "reejubhai",
+    },
+    {
+        "name": "Suddhabrato Ghosh",
+        "year": 2024,
+        "handle": "psychedelickid",
+    },
+    {
+        "name": "SUBHAJIT SAMANTA",
+        "year": 2024,
+        "handle": "Subhajit022",
+    },
+    {
+        "name": "Iman Kalyan Majumder",
+        "year": 2024,
+        "handle": "IKMajumder",
+    },
+    {
+        "name": "Ritwik Kundu",
+        "year": 2024,
+        "handle": "rick3300",
+    },
+    {
+        "name": "Ankush Datta",
+        "year": 2024,
+        "handle": "sardonyx",
+    },
+    {
+        "name": "Suvrajit Mullick",
+        "year": 2024,
+        "handle": "Suvo_007",
+    },
+    {
+        "name": "HARISH AMAN",
+        "year": 2024,
+        "handle": "JLKryptonite",
+    },
+    {
+        "name": "Neeraj Pratap Hazarika",
+        "year": 2024,
+        "handle": "NeerajHazarika",
+    },
+    {
+        "name": "Chandresh Patel",
+        "year": 2024,
+        "handle": "Chand1947",
+    },
+    {
+        "name": "KONETI CHANDRAKANT",
+        "year": 2024,
+        "handle": "konetichandrakant",
+    },
+    {
+        "name": "Sainath Tallam",
+        "year": 2023,
+        "handle": "SainathTallam",
+    },
+    {
+        "name": "PENUGONDA S M S N RAJESH",
+        "year": 2024,
+        "handle": "rajeshpenugonda6",
+    },
+    {
+        "name": "Nishant Bhadana",
+        "year": 2024,
+        "handle": "nishant_bhadana",
+    },
+    {
+        "name": "Hrittik Bania",
+        "year": 2024,
+        "handle": "Diamond_21Aug",
+    },
+    {
+        "name": "Abhash kumar",
+        "year": 2024,
+        "handle": "abhash6406",
+    },
+    {
+        "name": "Subham Bhansali",
+        "year": 2024,
+        "handle": "Subham4",
+    },
+    {
+        "name": "Sreeja Reddy Goli",
+        "year": 2024,
+        "handle": "shrijareddy",
+    },
+    {
+        "name": "Tathagata Ghosh",
+        "year": 2024,
+        "handle": "tathagata_010",
+    },
+    {
+        "name": "Sourit Sarkar",
+        "year": 2024,
+        "handle": "sourit_",
+    },
+    {
+        "name": "Anushuya Patra",
+        "year": 2024,
+        "handle": "anu_1804",
+    },
+    {
+        "name": "Vardhan Kumar Kothapally",
+        "year": 2024,
+        "handle": "vardhan_19",
+    },
+    {
+        "name": "Souvik Dey",
+        "year": 2024,
+        "handle": "sd111000",
+    },
+    {
+        "name": "Kevin Kingslin",
+        "year": 2024,
+        "handle": "CrimsonNinja",
+    },
+    {
+        "name": "KODAVALI SRI SURYA VAMSI",
+        "year": 2024,
+        "handle": "k.srisuryavamsi",
+    },
+    {
+        "name": "Anoushka Patra",
+        "year": 2024,
+        "handle": "purpleberry3.14",
+    },
+    {
+        "name": "Divi Srivastava",
+        "year": 2024,
+        "handle": "mass_2701",
+    },
+    {
+        "name": "RUDRESHWAR BARANWAL",
+        "year": 2024,
+        "handle": "RUDRA_2812",
+    },
+    {
+        "name": "Jaji Pravallika Gottapu",
+        "year": 2024,
+        "handle": "Jaji_Pravallika",
+    },
+    {
+        "name": "Prince Kumar",
+        "year": 2024,
+        "handle": "Unstoppable_1",
+    },
+    {
+        "name": "Kamal Pathak",
+        "year": 2023,
+        "handle": "DAT_BOI",
+    },
+    {
+        "name": "Monalisa Das",
+        "year": 2024,
+        "handle": "monali",
+    },
+    {
+        "name": "Vishal",
+        "year": 2024,
+        "handle": "Rockypil321",
+    },
+    {
+        "name": "Sai Teja Behera",
+        "year": 2024,
+        "handle": "sai_teja.",
+    },
+    {
+        "name": "Shreyoshi Chakraborty",
+        "year": 2024,
+        "handle": "shreyoshi5000",
+    },
+    {
+        "name": "Bikramjit Saha",
+        "year": 2024,
+        "handle": "biki_coder",
+    },
+    {
+        "name": "Adityaraj Debath",
+        "year": 2024,
+        "handle": "ARD1205",
+    },
+    {
+        "name": "Raunak kumar",
+        "year": 2024,
+        "handle": "Risky_1930",
+    },
+    {
+        "name": "VISHAL BHANJA",
+        "year": 2024,
+        "handle": "code_vishal18",
+    },
+    {
+        "name": "Subhrajit Dey",
+        "year": 2024,
+        "handle": "Subhrajit123",
+    },
+    {
+        "name": "Akhil Khader",
+        "year": 2024,
+        "handle": "akhbisk",
+    },
+    {
+        "name": "Sejal Shreya",
+        "year": 2024,
+        "handle": "sejal_shreya",
+    },
+    {
+        "name": "Sujal Modanwal",
+        "year": 2023,
+        "handle": "summan_gud",
+    },
+    {
+        "name": "Subhadeep Das",
+        "year": 2024,
+        "handle": "subhadeepdas20011029",
+    },
+    {
+        "name": "Rishita Shaw",
+        "year": 2024,
+        "handle": "rxshxta_",
+    },
+    {
+        "name": "Karra Ashish Reddy",
+        "year": 2025,
+        "handle": "AshishLimitless_4480",
+    },
+    {
+        "name": "KINGSHUK BARUA",
+        "year": 2023,
+        "handle": "Kingshuk_31",
+    },
+    {
+        "name": "Nikhil Joshi",
+        "year": 2023,
+        "handle": "nickjosi",
+    },
+    {
+        "name": "Manav Sarkar",
+        "year": 2024,
+        "handle": "manav_sarkar",
+    },
+    {
+        "name": "SANMAI REDDY YAMAGONDA",
+        "year": 2025,
+        "handle": "sanmai_reddy123",
+    },
+    {
+        "name": "Vinit Ranjan",
+        "year": 2025,
+        "handle": "Vinitranjansah",
+    },
+    {
+        "name": "Atrayee Samanta",
+        "year": 2025,
+        "handle": "atrayee03",
+    },
+    {
+        "name": "Aviral Yadav",
+        "year": 2025,
+        "handle": "aviral_yadav",
+    },
+    {
+        "name": "Sanghamitra De",
+        "year": 2025,
+        "handle": "thevyomonaut",
+    },
+    {
+        "name": "Dwaipayan Biswas",
+        "year": 2025,
+        "handle": "Debcode",
+    },
+    {
+        "name": "Arvind Kumar",
+        "year": 2025,
+        "handle": "arvind0266",
+    },
+    {
+        "name": "Arvind Kumar",
+        "year": 2025,
+        "handle": "arvind0266",
+    },
+    {
+        "name": "Sai Rugved",
+        "year": 2025,
+        "handle": "rugved.27",
+    },
+    {
+        "name": "Abhijit Saha",
+        "year": 2025,
+        "handle": "abhijitsaha",
+    },
+    {
+        "name": "Dhruv Jhunjhunwala",
+        "year": 2025,
+        "handle": "dhruvj25",
+    },
+    {
+        "name": "SAYANTAN PAL",
+        "year": 2025,
+        "handle": "Sayantan627",
+    },
+    {
+        "name": "Prinshu kumar",
+        "year": 2025,
+        "handle": "prinshuPK",
+    },
+    {
+        "name": "Anupriya",
+        "year": 2025,
+        "handle": "Anupriya_2.8",
+    },
+    {
+        "name": "Ankit Kumar",
+        "year": 2025,
+        "handle": "codewithankit",
+    },
+    {
+        "name": "Ankit Mishra",
+        "year": 2025,
+        "handle": "ankitm03012004",
+    },
+    {
+        "name": "Sayantan Datta",
+        "year": 2025,
+        "handle": "Sayantan07",
+    },
+    {
+        "name": "Vaibhav Singh",
+        "year": 2025,
+        "handle": "star_saber",
+    },
+    {
+        "name": "Anurag Banerjee",
+        "year": 2025,
+        "handle": "anurag_8b",
+    }
+]
+
+DATA = {r["handle"]: r for r in RAW_DATA}
+HANDLES_LIST = ';'.join([r["handle"] for r in RAW_DATA])
+#  anishde85,manish.17,coder_sounak
---
+++ utils/cf.py
@@ -0,0 +1,46 @@
+import requests
+
+URL = "https://codeforces.com/api/contest.standings?contestId=CONTEST_ID&handles="
+
+
+def format_name(name, handle):
+    fname, lname = name.split(' ', 1)
+    return fname + ' ' + handle + ' ' + lname
+
+
+def get_contest_data(contest_id, handles_str):
+    """Get the contest data from Codeforces API."""
+    url = URL.replace("CONTEST_ID", str(contest_id))
+    url += handles_str
+    response = requests.get(url)
+    return response.json()
+
+
+def filter_top_participants(contest_data, participants_info):
+    """Filter the top participants."""
+    participants = contest_data['result']['rows']
+
+    overall = []
+    top2ndYear = []
+    top1stYear = []
+    topFemales = []
+
+    for participant in participants[:3]:
+        handle = participant['party']['members'][0]['handle']
+        overall.append(format_name(participants_info[handle]['name'], handle))
+
+    for participant in participants:
+        handle = participant['party']['members'][0]['handle']
+        if participants_info[handle]['year'] == 2025:
+            top1stYear.append(format_name(
+                participants_info[handle]['name'], handle))
+        elif participants_info[handle]['year'] == 2024:
+            top2ndYear.append(format_name(
+                participants_info[handle]['name'], handle))
+
+    return {
+        'overall': overall,
+        'top2ndYear': top2ndYear[:3],
+        'top1stYear': top1stYear[:3],
+        'topFemales': topFemales[:3]
+    }
